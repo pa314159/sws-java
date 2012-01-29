987421347b88657a3af19bf11ea58196e16bf1c6
PAPPY <pa314159@gmail.com>
2012-01-29 17:56:20 +0200

    limit input when Content-Length is present in the request

--- http/src/main/java/net/pi/sws/http/HttpRequest.java
+++ http/src/main/java/net/pi/sws/http/HttpRequest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.Charset;
 
@@ -13,6 +14,55 @@ import net.pi.sws.io.ChannelInputStream;
 public class HttpRequest
 extends HttpMessage<ReadableByteChannel, InputStream, Reader>
 {
+
+	static public class LimitedChannelInput
+	implements ReadableByteChannel
+	{
+
+		private final ReadableByteChannel	in;
+
+		private int							expected;
+
+		public LimitedChannelInput( ReadableByteChannel in, int expected )
+		{
+			this.in = in;
+			this.expected = expected;
+		}
+
+		@Override
+		public void close() throws IOException
+		{
+			this.in.close();
+		}
+
+		@Override
+		public boolean isOpen()
+		{
+			return this.in.isOpen();
+		}
+
+		@Override
+		public int read( ByteBuffer dst ) throws IOException
+		{
+			if( this.expected == 0 ) {
+				return 0;
+			}
+
+			final int remaining = dst.remaining();
+
+			if( remaining > this.expected ) {
+				dst.limit( dst.position() + this.expected );
+			}
+
+			final int read = this.in.read( dst );
+
+			if( read > 0 ) {
+				this.expected -= read;
+			}
+
+			return read;
+		}
+	}
 
 	private final String	uri;
 
@@ -38,6 +88,20 @@ extends HttpMessage<ReadableByteChannel, InputStream, Reader>
 	Reader newCharStream( InputStream stream, Charset cs )
 	{
 		return new InputStreamReader( stream, cs );
+	}
+
+	@Override
+	ReadableByteChannel wrap( ReadableByteChannel channel ) throws IOException
+	{
+		final HttpHeader h = getHeader( HttpHeader.General.CONTENT_LENGTH );
+
+		if( h == null ) {
+			return channel;
+		}
+
+		final int expected = Integer.parseInt( h.getValue() );
+
+		return new LimitedChannelInput( channel, expected );
 	}
 
 }

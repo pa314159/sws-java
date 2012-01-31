062977bb776185b862f68f7b7f18d060d6d85f27
PAPPY <pa314159@gmail.com>
2012-01-31 17:49:04 +0200

    POST responds with a sample XML containing the request parameters and headers

--- test/src/main/java/net/pi/sws/test/PostMethod.java
+++ test/src/main/java/net/pi/sws/test/PostMethod.java
@@ -8,12 +8,26 @@ import java.net.URLDecoder;
 import java.util.Map;
 import java.util.TreeMap;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import net.pi.sws.http.HTTP;
+import net.pi.sws.http.HttpHeader;
+import net.pi.sws.http.HttpHeader.General;
 import net.pi.sws.http.HttpMethod;
 import net.pi.sws.http.HttpRequest;
 import net.pi.sws.http.HttpResponse;
 import net.pi.sws.io.IO;
 import net.pi.sws.util.ExtLog;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 @HTTP( "POST" )
 public class PostMethod
@@ -22,9 +36,57 @@ extends HttpMethod
 
 	static private final ExtLog	L	= ExtLog.get();
 
+	private Document			dom;
+
 	public PostMethod( HttpRequest request, HttpResponse response )
 	{
 		super( request, response );
+	}
+
+	protected Element addElement( String name ) throws IOException
+	{
+		final Element child = getDocument().createElement( name );
+
+		this.dom.appendChild( child );
+
+		return child;
+	}
+
+	protected Element addElement( String name, Element element ) throws IOException
+	{
+		final Element child = getDocument().createElement( name );
+
+		element.appendChild( child );
+
+		return child;
+	}
+
+	protected Element addElement( String name, String value, Element element ) throws IOException
+	{
+		final Element child = getDocument().createElement( name );
+
+		element.appendChild( child );
+		child.appendChild( getDocument().createTextNode( value ) );
+
+		return child;
+	}
+
+	protected Document getDocument() throws IOException
+	{
+		if( this.dom != null ) {
+			return this.dom;
+		}
+
+		try {
+			this.dom = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+
+			this.response.setHeader( new HttpHeader( General.CONTENT_TYPE, "text/xml" ) );
+
+			return this.dom;
+		}
+		catch( final ParserConfigurationException e ) {
+			throw new IOException( "Cannot create XML document", e );
+		}
 	}
 
 	@Override
@@ -54,6 +116,55 @@ extends HttpMethod
 		}
 
 		L.info( "%s", map );
+
+		this.response.setHeader( new HttpHeader( HttpHeader.General.CONTENT_TYPE, "text/xml" ) );
+
+		final Element top = addElement( "request" );
+
+		final Element headers = addElement( "headers", top );
+
+		for( final HttpHeader h : this.request.getHeaders() ) {
+			final Element header = addElement( "header", headers );
+
+			addElement( "name", h.getName(), header );
+
+			final Element values = addElement( "values", header );
+
+			for( final String v : h.getValues() ) {
+				addElement( "value", v, values );
+			}
+		}
+
+		final Element params = addElement( "parameters", top );
+
+		for( final Map.Entry<String, String> ent : map.entrySet() ) {
+			final Element param = addElement( "parameter", params );
+
+			addElement( "name", ent.getKey(), param );
+			addElement( "value", ent.getValue(), param );
+		}
+
+		sendDocument();
 	}
 
+	protected void sendDocument() throws IOException
+	{
+		if( this.dom == null ) {
+			throw new IllegalStateException( "No XML document has been requested" );
+		}
+
+		try {
+			final TransformerFactory f = TransformerFactory.newInstance();
+
+			f.setAttribute( "indent-number", new Integer( 2 ) );
+
+			final Transformer t = f.newTransformer();
+
+			t.setOutputProperty( OutputKeys.INDENT, "yes" );
+			t.transform( new DOMSource( this.dom ), new StreamResult( this.response.getCharStream( IO.UTF8 ) ) );
+		}
+		catch( final TransformerException e ) {
+			throw new IOException( "Cannot output XML document", e );
+		}
+	}
 }

88be5468efff2ec51470c6b8f27eb313e152fff9
PAPPY <pa314159@gmail.com>
2012-01-31 03:19:21 +0200

    added simple post method; playing with JS on SWS

--- test/web/default.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function() {
-	script = document.createElement("script");
-	
-	script.type = "text/javascript";
-	script.src = "prototype.js";
-	
-	document.body.appendChild(script);
-};

145fc6ff0e46d3694953839c99bfa34f689cf1eb
PAPPY <pa314159@gmail.com>
2012-02-24 12:42:35 +0200

    workaround for a milton bug that silently traps an IOexception while sending content

--- http-dav/src/main/java/net/pi/sws/dav/DavMethod.java
+++ http-dav/src/main/java/net/pi/sws/dav/DavMethod.java
@@ -1,7 +1,15 @@
 
 package net.pi.sws.dav;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import com.bradmcevoy.http.GetableResource;
+import com.bradmcevoy.http.Resource;
+import com.bradmcevoy.http.Response.Status;
+import com.bradmcevoy.http.exceptions.BadRequestException;
+import com.bradmcevoy.http.exceptions.NotAuthorizedException;
+import com.bradmcevoy.http.exceptions.NotFoundException;
 
 import net.pi.sws.http.HttpMethod;
 import net.pi.sws.http.HttpRequest;
@@ -21,14 +29,42 @@ extends HttpMethod<DavServiceFactory>
 	}
 
 	@Override
-	protected void respond() throws IOException
+	protected void respond()
+	throws IOException
 	{
 		final DavRequest davRequest = new DavRequest( this );
 		final DavResponse davResponse = new DavResponse( this );
 
-		davResponse.setCookie( "Cookie-1", "Value-1" );
-		davResponse.setCookie( "Cookie-2", "Value-2" );
+		if( "GET".equals( this.name ) ) {
+			final Resource res = this.fact.hm.getResourceFactory().getResource( null, davRequest.getAbsolutePath() );
 
-		this.fact.hm.process( davRequest, davResponse );
+			if( res == null ) {
+				davResponse.setStatus( Status.SC_NOT_FOUND );
+			}
+			else if( res instanceof GetableResource ) {
+				try {
+					final GetableResource getable = (GetableResource) res;
+
+					davResponse.setContentTypeHeader( getable.getContentType( null ) );
+
+					getable.sendContent( davResponse.getOutputStream(), null, null, null );
+				}
+				catch( final NotAuthorizedException e ) {
+					davResponse.setStatus( Status.SC_UNAUTHORIZED );
+				}
+				catch( final BadRequestException e ) {
+					davResponse.setStatus( Status.SC_BAD_REQUEST );
+				}
+				catch( final NotFoundException e ) {
+					davResponse.setStatus( Status.SC_NOT_FOUND );
+				}
+				catch( final FileNotFoundException e ) {
+					davResponse.setStatus( Status.SC_NOT_FOUND );
+				}
+			}
+		}
+		else {
+			this.fact.hm.process( davRequest, davResponse );
+		}
 	}
 }
